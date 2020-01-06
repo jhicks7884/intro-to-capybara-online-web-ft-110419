76c8c0f96e5a21489b27be7bf15fbb75ad145543
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-06 19:59:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,10 @@
 class Application < Sinatra::Base
   # Write your code here!
+
    get '/' do
      erb :index
    end
-
-   get '/' - greeting form
-     welcomes the user
-   end
-
-
+   
    get '/greet' do
      erb :greet
    end
